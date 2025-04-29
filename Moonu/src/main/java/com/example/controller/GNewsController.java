f5a3fda9e3c61b47da44265a5a7483d2af0ce2d0
Afrudeen S H <115522869+afrudeensh@users.noreply.github.com>
2025-04-29 06:39:07 +0530

    crossorigin for angular connection added

--- Moonu/src/main/java/com/example/controller/GNewsController.java
+++ Moonu/src/main/java/com/example/controller/GNewsController.java
@@ -5,12 +5,10 @@ import com.example.response.NewsResponse;
 import com.example.service.GNewsService;
 import com.example.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/gnews/v2")
 public class GNewsController {
     @Autowired
--- Moonu/src/main/java/com/example/controller/NewsApiController.java
+++ Moonu/src/main/java/com/example/controller/NewsApiController.java
@@ -5,12 +5,10 @@ import com.example.response.SourceResponse;
 import com.example.service.NewsService;
 import com.example.utils.NewsApiDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/news/v2")
 public class NewsApiController {
 
